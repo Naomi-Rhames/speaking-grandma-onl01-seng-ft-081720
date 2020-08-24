a5aaf785f2b4adae7174157b7b0561f61519849b
binding-proc-8938 <naomirhames2020@gmail.com>
2020-08-24 16:01:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ if phrase != phrase.upcase
   "HUH?! SPEAK UP, SONNY!"
 elsif phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == 
+  elsif phrase == 'WHAT DID YOU EAT TODAY?'
+   
   end
 end
 
